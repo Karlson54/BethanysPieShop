8b728dd4ee47673e2b516a16fe1332909c377a2b
Karlson54 <anruban2001@gmail.com>
2024-01-24 16:00:02 +0200

    adding models and services

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,6 +1,11 @@
+using BethanysPieShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+
+builder.Services.AddControllersWithViews(); // + MVC
 
 var app = builder.Build();
 
