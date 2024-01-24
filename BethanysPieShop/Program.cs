bd37868b3fec0d6c0cacd5e42acfe2f1305d60af
Karlson54 <anruban2001@gmail.com>
2024-01-24 23:12:15 +0200

    adding bootstrap, jquery

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
 builder.Services.AddScoped<IPieRepository, MockPieRepository>();
 
-builder.Services.AddControllersWithViews(); // + MVC
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
